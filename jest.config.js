d5dc14aecef01ace109d108630f357f50edbef00
JuneOrg2020 <jh.dev.acc.1026.i2k9@gmail.com>
2021-06-26 22:24:27 +0900

    add jest config file

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/test/styleMock.js"
+  }
+};
