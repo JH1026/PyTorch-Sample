bd16a560207b30df802c85f29e40cc1a4f066644
JuneOrg2020 <jh.dev.acc.1026.i2k9@gmail.com>
2021-06-28 01:08:15 +0900

    add selenium test

--- selenium/test.py
+++ selenium/test.py
@@ -1,26 +1,40 @@
 import json
 import time
 import chromedriver_binary
-from selenium import webdriver
+from selenium import webdriver 
 
+# config
 json_file = open('test_config.json', 'r')
 config = json.load(json_file)
+base_url = config['url']
+wait_time = float(config['wait_time'])
+drop_img_path = config['img_path']
+json_file.close()
 
 
-# WebDriver のオプションを設定する
 options = webdriver.ChromeOptions()
 # options.add_argument('--headless')
 
-print('connectiong to remote browser...')
+# page access
 driver = webdriver.Chrome(options=options)
+driver.get(base_url)
+time.sleep(wait_time)
 
-driver.get(config['url'])
-print(driver.current_url)
-time.sleep(float(config['wait_time']))
-
+# drop img
 element = driver.find_element_by_tag_name("input")
-element.send_keys(config['img_path'])
+element.send_keys(drop_img_path)
+time.sleep(0.25)
 
+# classify img
+buttons = driver.find_elements_by_tag_name("button")
+for button in buttons:
+    if button.text == "CLASSIFY":
+        button.click()
+        break
 
-# ブラウザを終了する
-# driver.quit()
+time.sleep(1.25)
+
+# check output
+element = driver.find_element_by_class_name("content-area")
+assert '%' in element.text
+driver.quit()
